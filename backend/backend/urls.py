62470c1ad9ffc824da7b42ab2b4783c16e14f110
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 06:16:13 +0330

    resolve a bug

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tweets', include('tweets.urls'))
+    path('api/tweets/', include('tweets.urls'))
 ]
