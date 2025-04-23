b4d494d825136a10cd9592036a624dea8f0f9a7f
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 05:28:36 +0330

    Make views

--- backend/tweets/views.py
+++ backend/tweets/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Tweet, Like
+from .serializers import TweetSerializer, LikeSerializer
 
-# Create your views here.
+class TweetView(APIView):
+    def get(self, request):
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TweetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class LikeTweetView(APIView):
+    def post(self, request, tweet_id):
+        tweet = Tweet.objects.get(id=tweet_id)
+        like = Like.objects.create(tweet=tweet, user=request.user)
+        return Response(LikeSerializer(like).data, status=201)
