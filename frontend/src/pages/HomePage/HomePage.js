8f200759bfa315fc327d419d53169cd157ffdb52
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 06:28:57 +0330

    edited project folder structure

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './css/HomePage.css';
+import './HomePage.css';
 
 function HomePage() {
   const [username, setUsername] = useState('');
