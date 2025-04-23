028ebe0919c23e7e216b2e33ba62b05c24f4a9a4
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 05:19:55 +0330

    make model

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
