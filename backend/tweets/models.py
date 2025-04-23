e80cd04e9d589a025d4d7c51801f06a78ea1ae52
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 05:25:51 +0330

    add __str__ function

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -5,6 +5,10 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=280)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username}: {self.content[:30]}"
+
 
 class Like(models.Model):
     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
