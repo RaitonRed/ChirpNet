d4f20e69fdc015783311c42ebad99b5449f7c70f
Ali.Reza <alirzahosinopoor.1390@gmail.com>
2025-04-23 06:19:30 +0330

    make a empty tweets feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-const tweets = [
-  { id: 1, username: 'RaitonRed', content: 'سلام دنیا! این اولین توییته 🐥' },
-  { id: 2, username: 'CodeMaster', content: 'هوای یادگیری برنامه‌نویسی دارید؟ بیاید توی ChirpNet!' },
-];
+import axios from 'axios';
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/tweets/')
+      .then(res => setTweets(res.data))
+      .catch(err => console.error('Error fetching tweets:', err));
+  }, []);
+
   return (
     <div className="app">
-      <h1 className="title">توییت‌ها</h1>
+      <h1 className="title">Tweets</h1>
       <div className="tweet-list">
         {tweets.map(tweet => (
           <div className="tweet" key={tweet.id}>
